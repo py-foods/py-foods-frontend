ecec7ae607a941c7fba3d272df2948dbea4c6a98
ltbhoan <ltbhoan@hanatourjsv.com>
2020-04-30 23:09:53 +0700

    add fb plugin

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@
       <router-view/>
       <br>
       <app-footer></app-footer>
-      <div id = "fb-root"></div>
       <div id="click-to-top">
-         <div class="fb-customerchat" attribution=setup_tool page_id="197657473618542" theme_color="#0084ff"></div>
          <div class="mouse">
             <button @click="scrollToTop" class="mouse-icon">
                <div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
