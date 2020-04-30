4a61be8a0530d78b65f1a468400163d354e56ea2
ltbhoan <ltbhoan@hanatourjsv.com>
2020-04-30 16:48:18 +0700

    add server.js for deploy

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const serverStatic = require('serve-static')
+const path = require('path')
+const app = express()
+app.use('/', serverStatic(path.join(__dirname, '/dist')))
+const port = process.env.port || 8081
+app.listen(port)
+console.log('listerning on port: ' + port)
